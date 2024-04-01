db540aee4f0f7e1805a6267cc9e6d4459373c494
Yordi Montero <monterocalvoyordi@gmail.com>
2024-04-01 17:01:35 -0600

    Changes on HomeScreen

---
+++ app/src/main/java/com/example/jetpackcomposetest/components/CustomButton.kt
@@ -0,0 +1,112 @@
+package com.example.jetpackcomposetest.components
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposetest.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomButton(
+    text: String = "Sign up with Google",
+    loadingText: String = "Creating account...",
+    icon: Painter = painterResource(id = R.drawable.ic_check_circle),
+    onClick: (() -> Unit)? = null
+) {
+
+    var clicked by remember { mutableStateOf(false) }
+
+    Row(modifier = Modifier.wrapContentWidth().wrapContentHeight()) {
+
+        Surface(
+            onClick = { clicked = !clicked },
+            shape = Shapes().medium,
+            border = BorderStroke(width = 1.dp, color = Color.LightGray),
+            color = MaterialTheme.colorScheme.surface
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .padding(
+                        start = 12.dp,
+                        end = 16.dp,
+                        top = 12.dp,
+                        bottom = 12.dp
+                    )
+                    .animateContentSize(
+                        animationSpec = tween(
+                            durationMillis = 300,
+                            easing = LinearOutSlowInEasing
+                        )
+                    )
+            ) {
+
+                Icon(
+                    painter = icon,
+                    contentDescription = "Null",
+                    //tint = Color.Unspecified
+                    tint = Color.Green
+                )
+
+                Spacer(
+                    modifier = Modifier.width(8.dp)
+                )
+
+                Text(text = if (clicked) loadingText else text)
+
+                if (clicked) {
+                    Spacer(
+                        modifier = Modifier.width(16.dp)
+                    )
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .width(16.dp)
+                            .height(16.dp),
+                        strokeWidth = 2.dp,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                    onClick?.invoke()
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
+@Composable
+@Preview(showSystemUi = true, showBackground = true)
+private fun PreviewGoogleButton() {
+    CustomButton()
+}
---
+++ app/src/main/java/com/example/jetpackcomposetest/components/CustomSpacer.kt
@@ -0,0 +1,18 @@
+package com.example.jetpackcomposetest.components
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+
+@Composable
+fun CustomSpacer(
+    width: Dp,
+    height: Dp
+) {
+    Spacer(
+        modifier = Modifier.width(width).height(height)
+    )
+}
